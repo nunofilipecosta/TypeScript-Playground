e041ad153c3019ccb160f3f02d04a3105128e940
Nuno Costa <ncosta@equalexperts.com>
2018-05-29 14:42:03 +0100

    01

---
+++ 01/src/Greeter.ts
@@ -0,0 +1,14 @@
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet() {
+    return 'Hello, ' + this.greeting;
+  }
+}
+
+var greeter = new Greeter('Nuno');
+
+greeter.greet();
